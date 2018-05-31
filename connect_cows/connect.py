1db699e5549bf08874c768a9d4cbc477f2ead419
lostsummer <lostsummer@gmail.com>
2018-05-31 13:44:34 +0800

    less code and rename

--- connect_cows/connect.py
+++ connect_cows/connect.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import collections
 
 Point = collections.namedtuple('Point', ['id', 'x', 'y'])
@@ -12,29 +11,23 @@ start = Point(id=0, x=0, y=0)
 
 def readCows():
     with open(input_file_path, 'r') as f:
-        return {Point(i, *[int(s)
-                            for s in f.readline().split()])
-                for i in range(1, int(f.readline()) + 1)}
+        return {Point(i+1, *[int(s) for s in f.readline().split()]) for i in range(int(f.readline()))}
 
 def walkRound(rest, past=[start.id], c=start):
-    isConnectable = lambda n: c.x == n.x or c.y == n.y
-    toPoint = lambda n: (rest.difference([n]), past + [n.id])
-    if not rest and isConnectable(start):
+    connectable = lambda n: c.x == n.x or c.y == n.y
+    moveto = lambda n: (rest.difference([n]), past + [n.id])
+    if not rest and connectable(start):
         yield ' -> '.join([str(i) for i in past + [start.id]])
-    for n in rest:
-        if isConnectable(n):
-            yield from walkRound(*toPoint(n), n)
+    for n in filter(connectable, rest):
+        yield from walkRound(*moveto(n), n)
 
 def writeOut(num):
     with open(output_file_path, 'w') as f:
         f.write('{}\n'.format(num))
 
 if __name__ == "__main__":
-    cows = readCows()
-    routes = [r for r in walkRound(cows)]
+    routes = [r for r in walkRound(readCows())]
     num = len(routes)
     writeOut(num)
     print("number of routes: {}".format(num))
-    for r in routes:
-        print(r)
-
+    [print(r) for r in routes]
