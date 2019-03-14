2b151b7a836c6f2067b9a241129836e6b93e6f43
yushin <lostsummer@gmail.com>
2019-03-14 03:34:05 -0700

    组合首元素最大值处缩小计算规模

--- c_10_sum_100/c1.py
+++ c_10_sum_100/c1.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
-BIGGEST = 100 - sum(range(1, 10))
+import itertools
 
-def fetch(current=0, selected=[]):
+"""
+算法有两个重要的优化点:
+    1. 组合中的最大元素BIGGEST有限制，也就是考虑前9个数和最小的情况
+    2. 组合按生序排列的首元素有最大限制，首元素是我们递归试算的起始点
+"""
+
+BIGGEST = 100 - sum(range(1, 10))
+MAX_HEAD = list(itertools.takewhile(lambda x:sum(x)<=100, (range(1, 101)[i:i+10] for i in range(0, 100))))[-1][0]
+
+def fetch(current, selected=[]):
     if current <= BIGGEST and len(selected) < 10:
-        if current > 0:
-            selected_ = selected + [current]
-        else:
-            selected_ = selected + []
+        selected_ = selected + [current]
         sum_val = sum(selected_)
         n = len(selected_)
         if sum_val == 100 and n == 10:
             yield selected_
         elif sum_val < 100 and n < 10:
-            for i in range(current+1, 100 - sum_val + 1):
+            for i in range(current+1, 100-sum_val+1):
                 yield from fetch(i, selected_)
 
-choices = [i for i in fetch(0)]
-#print("number of choices: {}".format(len(choices)))
-for i in choices:
-    print(','.join([str(i) for i in i]))
+choices = [c for h in range(1, MAX_HEAD + 1) for c in fetch(h)]
+
+for c in choices:
+    print(','.join(str(i) for i in c))
